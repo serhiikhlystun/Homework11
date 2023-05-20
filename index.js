b4db898d691652e38d67098bf0bcbfb7fda0988e
Serhii <s.khlistyn@gmail.com>
2023-05-21 00:04:24 +0300

    Add eleventh homework(add lodash and jquery libraries

---
+++ index.js
@@ -0,0 +1,14 @@
+// import { name } from "./module";
+// import './style.css';
+
+// console.log("Your name is: " + name);
+
+// var validator = require('validator');
+
+// console.log(validator.isEmail('foo@bar.com'));
+// console.log(validator.isURL("foo@sdf.co"));
+// console.log(validator.toDate("12.12.2000"));
+
+// var moment = require('moment');
+
+// console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
